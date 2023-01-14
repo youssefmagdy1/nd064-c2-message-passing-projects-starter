3aafcfc026d43446edf3c864075e18e57a8d42ec
Youusef Magdy <youssef.magdy.775@gmail.com>
2023-01-14 17:43:27 +0200

    edit grpc server

--- modules/persons-api/app/__init__.py
+++ modules/persons-api/app/__init__.py
@@ -25,6 +25,8 @@ def create_app(env=None):
     from app.config import config_by_name
     from app.routes import register_routes
 
+    gRPCserver()
+
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
     api = Api(app, title="UdaConnect API", version="0.1.0")
@@ -33,7 +35,6 @@ def create_app(env=None):
 
     register_routes(api, app)
     db.init_app(app)
-    gRPCserver()
 
     @app.route("/health")
     def health():
